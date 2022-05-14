155833c52aa8a09ccbc14972e1ad525225fbd3a2
Alex Ko <help.mail015@gmail.com>
2022-05-14 23:46:35 +0300

    add Lesson 04

---
+++ Lesson04/Ex003/Program.cs
@@ -0,0 +1,12 @@
+﻿// Factorial рекурсия
+
+double Factorial (int n)
+{
+	if(n == 1) return 1;
+	else return n * Factorial(n - 1);
+}
+Console.Clear();
+for (int i = 1; i < 40; i++)
+{
+	Console.WriteLine($"{i}! = {Factorial(i)}");
+}
